de1fbbeaeee6df8ade16e3e382a385cd687e0d56
Bogdan Shcherban <bshcherban@brightgrove.com>
2019-09-17 15:01:28 +0300

    build

---
+++ projects/qatar/build/precache-manifest.8ded25260721fd70d6c1b8b209f054bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0f1b5dd4b4e3e4f012cfbb626281f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "140bc5abd3776b4812e3",
+    "url": "/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "140bc5abd3776b4812e3",
+    "url": "/static/js/2.a9d7b96d.chunk.js"
+  },
+  {
+    "revision": "f3c2b8cc7aa38d7378a3",
+    "url": "/static/js/main.20603129.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "24d6dd0b98993f392fa81238cb96c088",
+    "url": "/static/media/background.24d6dd0b.jpg"
+  },
+  {
+    "revision": "ba0bd9def6fea08d26a8e394d8410c25",
+    "url": "/static/media/containerBackground.ba0bd9de.jpg"
+  },
+  {
+    "revision": "4ab4fd16f913db38b25ba45f650cda45",
+    "url": "/static/media/logo-header.4ab4fd16.png"
+  },
+  {
+    "revision": "0b8bcf1f1965c0976cde9145eafaf5cc",
+    "url": "/static/media/logo-title.0b8bcf1f.png"
+  },
+  {
+    "revision": "93d3c4cef96b790d6525a6823fba6464",
+    "url": "/static/media/pulsetile-logo.93d3c4ce.png"
+  },
+  {
+    "revision": "eef539fe5e75bd20f087e50ae8c9ba7a",
+    "url": "/static/media/randomPatient.eef539fe.png"
+  },
+  {
+    "revision": "f15dbaaeb7cb20132c96ba6a70102edd",
+    "url": "/static/media/ripple-foundation-logo-footer.f15dbaae.png"
+  }
+]);
